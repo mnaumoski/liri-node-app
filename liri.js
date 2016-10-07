8a23528c08a24a1797fa26cec7c71107ac06a808
mnaumoski <naumoski.marija@gmail.com>
2016-10-07 13:06:50 -0400

    done with movies

--- liri.js
+++ liri.js
@@ -1,13 +1,37 @@
+//get node modules
 var request = require('request');
+var getTwitter = require('twitter');
+var getSpotify = require('spotify');
+var keys = require('./keys.js');
+
 //liri will have several commands; use switch
 var command = process.argv[2];
 
 switch (command) {
-    case "movie-this":// `node liri.js movie-this '<movie name here>'`
-        var movieName = process.argv[3]; //input name of movie
-        var queryUrl = 'http://www.omdbapi.com/?t=' + movieName + '&y=&plot=short&r=json&tomatoes=true';
-        request(queryUrl, function(error, response, body) {
-                    if (!error && response.statusCode == 200);
+  case "movie-this":// `node liri.js movie-this '<movie name here>'`
+    findMovie();
+    break;
+  case "my-tweets":
+
+        var twitterConsumerKey = keys.twitterKeys;
+        console.log(twitterConsumerKey);
+        break;
+                    // case "spotify-this-song":
+                    // break;
+
+                    // case "do-what-it-says":
+
+                    // break;
+
+                    default:
+                    text = "Looking forward to the Weekend";
+                };
+
+function findMovie() {
+  var movieName = process.argv[3]; //input name of movie
+  var queryUrl = 'http://www.omdbapi.com/?t=' + movieName + '&y=&plot=short&r=json&tomatoes=true';
+  request(queryUrl, function(error, response, body) {
+                    if (!error && response.statusCode == 200){
                     // title of movie
                     console.log("The title of the movie is " + JSON.parse(body)['Title']);
                     // year of release
@@ -27,47 +51,14 @@ switch (command) {
                     // rotten tomatoes URL
                     console.log("Rotten tomatoes URL: " + JSON.parse(body)['tomatoURL']);
 
-                        });
+                       }
+                     })
                   // when user search is empty
-                  if (movieName === null || movieName === " ") {
-                          
-                        queryUrl = 'http://www.omdbapi.com/?t=Mr.+Nobody&y=&plot=short&r=json';
-
-                        request(queryUrl, function(error, response, body) {
-                                if (!error && response.statusCode == 200);
-                                console.log(queryUrl); 
-                                console.log(JSON.parse(body)['Title']);
-                                console.log("Country: " + " has been produced in " + JSON.parse(body)['Country'] + ".")
-                              })
-                            };
-                    break;
-    case "my-tweets":
-        var getKeys = require('./keys.js');
-        var tweets = process.argv[3];
-        var a = ('/twitterKeys.consumer_key;')
-        var b = twitterKeys.consumer_secret;
-        var c = twitterKeys.access_token_key;
-        var d = twitterKeys.access_token_secret;
-
-        console.log(a);
-
-
-        var twitter = require('twitter');
-
-// console.log("=======");
-// console.log(getKeys);
-
-// console.log(getKeys.twitterKeys);
-
-
-        break;
-                    // case "spotify-this-song":
-                    // break;
-
-                    // case "do-what-it-says":
-
-                    // break;
-
-                    default:
-                    text = "Looking forward to the Weekend";
-                }
+  if (movieName === null || movieName === "") {
+    request('http://www.omdbapi.com/?t=Mr.+Nobody&y=&plot=short&r=json', function(error, response, body) {
+        if (!error && response.statusCode == 200) {
+                                console.log("The title of the movie is " + JSON.parse(body)['Title']);
+                            }
+                      });
+                    }
+                  }
